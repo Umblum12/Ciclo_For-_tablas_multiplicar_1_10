e323c06e57fba9cea7f661094192d507a7a60ab1
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:26:44 -0600

    Agregar archivos de proyecto.

---
+++ Ciclo_For[_tablas_multiplicar_1_10/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n, i;
+Console.WriteLine("Digite el valor de n: ");
+n = Convert.ToInt32(Console.ReadLine());
+for (i = 0; i<=10; i++)
+{
+    Console.WriteLine("{0} x {1} = {2}", n,i,(n*i));
+}
+Console.ReadKey(true);
